b369ca3c17a189eef4f0e6b005d7f434644328e1
EditCaps <l8620673@gmail.com>
2026-02-21 14:01:52 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.EDITCAPS_CONFIG = {
   supabaseUrl: 'https://floccoqkcgkgtmasjukb.supabase.co',
-  supabaseKey: 'sb_publishable_Nw4Qja-AZinwWEDj7oy9zA_fD6W-o8d',
-  paypalClientId: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsb2Njb3FrY2drZ3RtYXNqdWtiIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MTY2MjExMiwiZXhwIjoyMDg3MjM4MTEyfQ.4J18vXkdh04wmhqsRzfZYp79xwC6sxGxhgwGMSug9z8'
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsb2Njb3FrY2drZ3RtYXNqdWtiIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MTY2MjExMiwiZXhwIjoyMDg3MjM4MTEyfQ.4J18vXkdh04wmhqsRzfZYp79xwC6sxGxhgwGMSug9z8',
+  paypalClientId: 'ATMeQVKdjHls1DV6xgcQFFyfy5D6XIY8_FEeTCY9nUPuESstJMgufD-OWsDRSLdyBHw90eav_3rOyR7Z'
 };
