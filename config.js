d66022dda44954f44ceb3a73e3e7be049c7b636c
EditCaps <l8620673@gmail.com>
2026-02-21 13:39:43 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.EDITCAPS_CONFIG = {
   supabaseUrl: 'https://floccoqkcgkgtmasjukb.supabase.co',
-  supabaseKey: 'sb_publishable_Nw4Qja-AZinwWEDj7oy9zA_fD6W-o8d'
+  supabaseKey: 'sb_publishable_Nw4Qja-AZinwWEDj7oy9zA_fD6W-o8d',
   paypalClientId: 'ATMeQVKdjHls1DV6xgcQFFyfy5D6XIY8_FEeTCY9nUPuESstJMgufD-OWsDRSLdyBHw90eav_3rOyR7Z'
 };
